d58f6d8434855309a2ec8ce6558971deb5f691ff
Joanne P Silva <joannepassos@gmail.com>
2021-06-10 09:04:59 -0300

    Java_Foundations
    Jogo Old gamer
    Exercícios

---
+++ ProblemSet2_1/ProblemSet2_1/src/problemset2_1/ProblemSet2_1.java
@@ -0,0 +1,14 @@
+package problemset2_1;
+
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        
+       
+        for (String smile : smile) {
+            smile = x;
+            System.out.println(smile);
+        }
+    
+        
+    }   
+}
