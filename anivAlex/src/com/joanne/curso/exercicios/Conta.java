6ea734d169f50f9a9c0991b1c74701d0637dddf3
Joanne P Silva <joannepassos@gmail.com>
2021-07-16 14:33:13 -0300

    findTotal | remove
    exercício: encontrar o valor total e remover 1 item da lista.

--- anivAlex/src/com/joanne/curso/exercicios/Conta.java
+++ anivAlex/src/com/joanne/curso/exercicios/Conta.java
@@ -8,11 +8,28 @@ public class Conta {
 
         for (int i = 0; i < valor.size() ; i++) {
            double taxas = valor.get(i) * 0.20;
-           double totaComTaxas = taxas + valor.get(i);
-            valorPerson.add(totaComTaxas);
+           double totalComTaxas = taxas + valor.get(i);
+            valorPerson.add(totalComTaxas);
         }
 
         return valorPerson;
+    }
+    public double findTotal(ArrayList<Double>contas){
+        double soma = 0;
+        for (int i = 0; i < contas.size(); i++) {
+            soma += contas.get(i);
+
+        }
+        return soma;
+    }
+
+    public int removePerson(ArrayList<Double>remover){
+        int personRemove = 0;
+        for (int i = 0; i < remover.size(); i++) {
+            personRemove -= remover.remove(i);
+
+        }
+        return personRemove;
     }
 
 }
--- anivAlex/src/com/joanne/curso/exercicios/Main.java
+++ anivAlex/src/com/joanne/curso/exercicios/Main.java
@@ -10,8 +10,16 @@ public class Main {
         Conta calcContas = new Conta();
         ArrayList<Double> valoresPorConvidado = new ArrayList(Arrays.asList(10.0, 12.0, 9.0, 8.0, 7.0, 15.0, 11.0, 30.0));
         ArrayList<Double>resultado = calcContas.totalContas(valoresPorConvidado);
-
+        
+       double total = calcContas.findTotal(resultado);
         System.out.println("O valor por convidado é: " + resultado);
-        System.out.println("O valor total da conta é de: "  );
+        System.out.println("O valor total da conta é de: " + total  );
+
+        double remove = valoresPorConvidado.remove(7);
+        System.out.println("O valor da conta removida é: " + remove);
+
+        double calcToRemove = total - remove;
+        System.out.println("o total com a conta removida ficou: " +calcToRemove );
+
     }
 }
